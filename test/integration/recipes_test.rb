bdbdb627560cbd042059315c55e79eef31fea2a0
SHERRY <swasieko@gmail.com>
2018-01-20 18:11:59 +0000

    Add ability to create new recipes with tests

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -4,7 +4,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
   
   def setup
     @chef = Chef.create!(chefname: "Test", email: "tester@test.com")
-    @recipe = Recipe.create!(name: "Sauteed Onions", description: "Sautee onions till clear", chef: @chef)
+    @recipe = Recipe.create(name: "Sauteed Onions", description: "Sautee onions till clear", chef: @chef)
     @recipe2 = @chef.recipes.build(name: "Grilled Onions", description: "grill for 5 minutes")
     @recipe2.save
   end
@@ -17,8 +17,43 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes list" do
     get recipes_path
     assert_template 'recipes/index'
-    assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
+  end
+  
+  test "should get recipes show" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_match @recipe.name.titleize, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
+  end
+  
+  test "create new valid recipe" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = "chicken sautee"
+    description_of_recipe = "stir in vegetables and simmmer for 5 minutes"
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe } }
+    end
+    follow_redirect!
+    assert_match name_of_recipe.titleize, response.body
+    assert_match description_of_recipe, response.body
+  end
+  
+  test "reject invalid recipe submissions" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: " ", description: " " } }
+    end
+    assert_template 'recipes/new'
+    assert_select 'h4.panel-title'
+    assert_select 'div.panel-body'
   end
   
 end
+
+
+
