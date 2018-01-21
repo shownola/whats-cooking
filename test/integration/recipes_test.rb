a415bad4c359c02a4c00aa149f4068dcfeb97a58
SHERRY <swasieko@gmail.com>
2018-01-21 04:30:03 +0000

    Refactored new and edit pages and recipe controller

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -29,6 +29,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @chef.chefname, response.body
     assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this Recipe"
     assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this Recipe"
+    assert_select 'a[href=?]', recipes_path, "Return to Recipes"
   end
   
   test "create new valid recipe" do
